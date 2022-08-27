08819bcdd8dfb420b8f234b2ec79c0a9b37892ff
Dsoftwaredev <danielsoftwaredev@outlook.com>
2022-08-27 18:17:49 +0100

    setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Backgroundstate from './components/Backgroundstate';
+import Toggle from './components/Toggle';
+
+function App() {
+  return (
+    <div className="App">
+     <Toggle />
+     <Backgroundstate />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Backgroundstate.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+export default function Backgroundstate() {
+  // call useState and assign its return values to `color` and `setColor`
+const [color, setColor] = useState();
+ const divStyle = {backgroundColor: color};
+
+  return (
+    <div style={divStyle}>
+      <p>The color is {color}</p>
+      <button onClick={() => setColor("Aquamarine")}>
+        Aquamarine
+      </button>
+      <button onClick={() => setColor("BlueViolet")}>
+        BlueViolet
+      </button>
+      <button onClick={() => setColor("Chartreuse")}>
+        Chartreuse
+      </button>
+      <button onClick={() => setColor("CornflowerBlue")}>
+        CornflowerBlue
+      </button>
+    </div>
+  );
+}
