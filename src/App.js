aca736eefe349465db6e9d5800e4bdfd5fb56804
Dsoftwaredev <danielsoftwaredev@outlook.com>
2022-08-27 23:24:09 +0100

    basic app built

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-
-import './App.css';
-import Backgroundstate from './components/Backgroundstate';
-import Toggle from './components/Toggle';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { AddThoughtForm } from './components/AddThoughtForm';
+import { Thought } from './components/Thought';
+import { generateId, getNewExpirationTime } from './components/Utilities';
 
 function App() {
+  const [thoughts, setThoughts] = useState([
+    {
+      id: generateId(),
+      text: 'This is a place for your passing thoughts.',
+      expiresAt: getNewExpirationTime(),
+    },
+    {
+      id: generateId(),
+      text: "They'll be removed after 15 seconds.",
+      expiresAt: getNewExpirationTime(),
+    },
+  ]);
+const addThought = (thought) => {
+  setThoughts(prev => [thought, ...prev])
+}
+const removeThought = (thoughtIdToRemove) => {
+  setThoughts((prev) =>
+    prev.filter((thought) => thought.id !== thoughtIdToRemove)
+  );
+};
+
   return (
     <div className="App">
-     <Toggle />
-     <Backgroundstate />
+      <header>
+        <h1>Passing Thoughts</h1>
+      </header>
+      <main>
+        <AddThoughtForm addThought={addThought}/>
+        <ul className="thoughts">
+          {thoughts.map((thought) => (
+            <Thought key={thought.id} thought={thought} removeThought = {removeThought} />
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
-
 export default App;
